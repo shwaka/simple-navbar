1af3e776eae35b27926aa1054f700f9359d1854a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2025-12-06 14:05:26 +0900

    docs: add comments to local-test/vite.config.ts

--- test/local-test/vite.config.ts
+++ test/local-test/vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     dedupe: [
+      // Necessary only for local-test.
+      // This is because the dependency is written as follows:
+      //   "@shwaka/simple-navbar": "file:../../"
       "react",
       "react-dom",
     ],
