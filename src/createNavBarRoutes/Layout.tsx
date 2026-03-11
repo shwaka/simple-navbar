d5bb85f2973295c6a0494abd88d0be92a5d948f6
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-03-11 19:43:05 +0900

    feat: add color mode (experimental)

--- src/createNavBarRoutes/Layout.tsx
+++ src/createNavBarRoutes/Layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactElement } from "react"
 
-import { Box, type SxProps } from "@mui/material"
+import { Box, CssBaseline, type SxProps } from "@mui/material"
+import { ThemeProvider, createTheme, Theme } from "@mui/material/styles"
 import { Outlet } from "react-router"
 
 import { NavBar } from "../NavBar"
@@ -15,31 +16,58 @@ export interface LayoutProps {
   gitHubUrl?: string
   footerDisplay: "block" | "none"
   contentBoxSx?: SxProps
+  colorMode?: "light" | "dark"
 }
 
-export function Layout({ subpageRoutes, siteTitle, numberOfExplicitItems, gitHubUrl, footerDisplay, contentBoxSx }: LayoutProps): ReactElement {
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+  },
+})
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+})
+
+function getTheme(colorMode: "light" | "dark" | undefined): Theme {
+  switch (colorMode) {
+    case "light":
+    case undefined:
+      return lightTheme
+    case "dark":
+      return darkTheme
+  }
+}
+
+export function Layout({ subpageRoutes, siteTitle, numberOfExplicitItems, gitHubUrl, footerDisplay, contentBoxSx, colorMode }: LayoutProps): ReactElement {
   validateSubpageRoutes(subpageRoutes)
   const items = subpageRoutes.map((page) => ({
     path: page.path,
     text: page.name,
   }))
+  const colorTheme = getTheme(colorMode)
   return (
     <div>
-      <NavBar
-        title={siteTitle} items={items} gitHubUrl={gitHubUrl}
-        numberOfExplicitItems={numberOfExplicitItems}
-      />
-      <Box
-        sx={{
-          boxSizing: "border-box",
-          marginTop: "10px",
-          width: "100%", overflowX: "auto",
-          ...contentBoxSx,
-        }}
-      >
-        <Outlet />
-        <Footer subpageRoutes={subpageRoutes} display={footerDisplay} />
-      </Box>
+      <ThemeProvider theme={colorTheme}>
+        <CssBaseline />
+        <NavBar
+          title={siteTitle} items={items} gitHubUrl={gitHubUrl}
+          numberOfExplicitItems={numberOfExplicitItems}
+        />
+        <Box
+          sx={{
+            boxSizing: "border-box",
+            marginTop: "10px",
+            width: "100%", overflowX: "auto",
+            ...contentBoxSx,
+          }}
+        >
+          <Outlet />
+          <Footer subpageRoutes={subpageRoutes} display={footerDisplay} />
+        </Box>
+      </ThemeProvider>
     </div>
   )
 }
