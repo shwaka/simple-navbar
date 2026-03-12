a03710af101b09116cb0294634cf1746b59743ea
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-03-12 13:36:00 +0900

    feat: add icon to ColorModeSelector

--- src/NavBar/ColorModeSelector.tsx
+++ src/NavBar/ColorModeSelector.tsx
@@ -1,8 +1,26 @@
 import { type MouseEvent, useState, type ReactElement, Fragment, useCallback } from "react"
 
+import DarkMode from "@mui/icons-material/DarkMode"
 import LightMode from "@mui/icons-material/LightMode"
-import { IconButton, Menu, MenuItem } from "@mui/material"
+import SettingsBrightness from "@mui/icons-material/SettingsBrightness"
+import { IconButton, ListItemIcon, ListItemText, Menu, MenuItem } from "@mui/material"
 import { useColorScheme } from "@mui/material/styles"
+
+const colorModeMenuConfig = {
+  system: {
+    text: "System",
+    // MUI公式ドキュメントの system/light/dark 設定画面で使われてたアイコン
+    icon: (): ReactElement => (<SettingsBrightness fontSize="small" />),
+  },
+  light: {
+    text: "Light",
+    icon: (): ReactElement => (<LightMode fontSize="small" />),
+  },
+  dark: {
+    text: "Dark",
+    icon: (): ReactElement => (<DarkMode fontSize="small" />),
+  },
+} as const
 
 export function ColorModeSelector(): ReactElement {
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
@@ -40,7 +58,12 @@ export function ColorModeSelector(): ReactElement {
             onClick={() => setColorMode(m)}
             key={m}
           >
-            {m}
+            <ListItemIcon>
+              {colorModeMenuConfig[m].icon()}
+            </ListItemIcon>
+            <ListItemText>
+              {m}
+            </ListItemText>
           </MenuItem>
         ))}
       </Menu>
