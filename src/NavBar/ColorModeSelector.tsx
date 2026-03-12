d23c194af5d10aa34dadb896a789cff06e94bba1
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-03-12 10:44:35 +0900

    feat: add Menu to select color mode (WIP)

--- src/NavBar/ColorModeSelector.tsx
+++ src/NavBar/ColorModeSelector.tsx
@@ -1,12 +1,37 @@
-import type { ReactElement } from "react"
+import { type MouseEvent, useState, type ReactElement, Fragment } from "react"
 
 import LightMode from "@mui/icons-material/LightMode"
-import { IconButton } from "@mui/material"
+import { IconButton, Menu, MenuItem } from "@mui/material"
 
 export function ColorModeSelector(): ReactElement {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: MouseEvent<HTMLButtonElement>): void => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = (): void => {
+    setAnchorEl(null)
+  }
   return (
-    <IconButton>
-      <LightMode />
-    </IconButton>
+    <Fragment>
+      <IconButton onClick={handleClick}>
+        <LightMode />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            "aria-labelledby": "basic-button",
+          },
+        }}
+      >
+        <MenuItem onClick={handleClose}>System</MenuItem>
+        <MenuItem onClick={handleClose}>Light</MenuItem>
+        <MenuItem onClick={handleClose}>Dark</MenuItem>
+      </Menu>
+    </Fragment>
   )
 }
