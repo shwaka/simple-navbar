5ec1ae4b1c709ac6e0eaf4b30a08a9e37f81983a
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-03-12 09:52:05 +0900

    refactor: remove theme.ts

--- src/NavBar/index.tsx
+++ src/NavBar/index.tsx
@@ -1,12 +1,10 @@
 import type { ReactElement } from "react"
 
-import { ThemeProvider } from "@emotion/react"
-import { AppBar, Toolbar, useTheme } from "@mui/material"
+import { AppBar, Toolbar } from "@mui/material"
 
 import type { NavBarItem } from "./NavBarItem"
 import { NavBarItemsDesktop } from "./NavBarItemsDesktop"
 import { NavBarItemsMobile } from "./NavBarItemsMobile"
-import { createNavBarTheme } from "./theme"
 import { Title } from "./Title"
 
 interface NavBarProps {
@@ -17,22 +15,18 @@ interface NavBarProps {
 }
 
 export function NavBar({ title, items, gitHubUrl, numberOfExplicitItems }: NavBarProps): ReactElement {
-  const currentTheme = useTheme()
-  const theme = createNavBarTheme(currentTheme)
   return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="sticky" color="inherit">
-        <Toolbar disableGutters>
-          <NavBarItemsMobile
-            items={items} gitHubUrl={gitHubUrl}
-          />
-          <Title title={title} />
-          <NavBarItemsDesktop
-            items={items} gitHubUrl={gitHubUrl}
-            numberOfExplicitItems={numberOfExplicitItems}
-          />
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+    <AppBar position="sticky" color="inherit">
+      <Toolbar disableGutters>
+        <NavBarItemsMobile
+          items={items} gitHubUrl={gitHubUrl}
+        />
+        <Title title={title} />
+        <NavBarItemsDesktop
+          items={items} gitHubUrl={gitHubUrl}
+          numberOfExplicitItems={numberOfExplicitItems}
+        />
+      </Toolbar>
+    </AppBar>
   )
 }
