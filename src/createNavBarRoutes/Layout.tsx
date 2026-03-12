9f02f7fef67c4a040d0bf5b4c6dee217ad62ab97
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-03-12 11:17:44 +0900

    refactor: remove SelectColorMode (which was copied from MUI docs)

--- src/createNavBarRoutes/Layout.tsx
+++ src/createNavBarRoutes/Layout.tsx
@@ -1,8 +1,8 @@
-import type { ReactElement, ReactNode } from "react"
+import type { ReactElement } from "react"
 
 import { useTheme } from "@emotion/react"
-import { Box, CssBaseline, type SxProps, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
-import { ThemeProvider, createTheme, useColorScheme } from "@mui/material/styles"
+import { Box, CssBaseline, type SxProps } from "@mui/material"
+import { ThemeProvider, createTheme } from "@mui/material/styles"
 import { deepmerge } from "@mui/utils"
 import { Outlet } from "react-router"
 
@@ -19,44 +19,6 @@ export interface LayoutProps {
   footerDisplay: "block" | "none"
   contentBoxSx?: SxProps
   colorMode?: "light" | "dark"
-}
-
-function SelectColorMode(): ReactNode {
-  const { mode, setMode } = useColorScheme()
-  if (mode === undefined) {
-    return null
-  }
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-        bgcolor: "background.default",
-        color: "text.primary",
-        borderRadius: 1,
-        p: 3,
-        minHeight: "56px",
-      }}
-    >
-      <FormControl>
-        <FormLabel id="demo-theme-toggle">Theme</FormLabel>
-        <RadioGroup
-          aria-labelledby="demo-theme-toggle"
-          name="theme-toggle"
-          row
-          value={mode}
-          onChange={(event) =>
-            setMode(event.target.value as "system" | "light" | "dark")}
-        >
-          <FormControlLabel value="system" control={<Radio />} label="System" />
-          <FormControlLabel value="light" control={<Radio />} label="Light" />
-          <FormControlLabel value="dark" control={<Radio />} label="Dark" />
-        </RadioGroup>
-      </FormControl>
-    </Box>
-  )
 }
 
 export function Layout({ subpageRoutes, siteTitle, numberOfExplicitItems, gitHubUrl, footerDisplay, contentBoxSx }: LayoutProps): ReactElement {
@@ -92,7 +54,6 @@ export function Layout({ subpageRoutes, siteTitle, numberOfExplicitItems, gitHub
             ...contentBoxSx,
           }}
         >
-          <SelectColorMode />
           <Outlet />
           <Footer subpageRoutes={subpageRoutes} display={footerDisplay} />
         </Box>
