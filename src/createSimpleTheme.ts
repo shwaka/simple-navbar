423a8d2835b7f01bb5e3d03d68fee9cffe456cf5
Shun Wakatsuki <shun.wakatsuki@gmail.com>
2026-04-07 17:45:28 +0900

    feat: use OKLCH instead of HSL

--- src/createSimpleTheme.ts
+++ src/createSimpleTheme.ts
@@ -1,4 +1,5 @@
 import { createTheme, type Theme } from "@mui/material"
+import Color from "colorjs.io"
 
 interface CreateSimpleThemeArgs {
   mainHue: number
@@ -10,14 +11,14 @@ export function createSimpleTheme({ mainHue }: CreateSimpleThemeArgs): Theme {
       light: {
         palette: {
           primary: {
-            main: hslToHex(mainHue, 70, 40),
+            main: oklchToHex(0.60, 0.70, mainHue),
           },
         },
       },
       dark: {
         palette: {
           primary: {
-            main: hslToHex(mainHue, 70, 60),
+            main: oklchToHex(0.60, 0.70, mainHue),
           },
         },
       },
@@ -25,17 +26,8 @@ export function createSimpleTheme({ mainHue }: CreateSimpleThemeArgs): Theme {
   })
 }
 
-// Source - https://stackoverflow.com/a/44134328
-// Posted by Juraj, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-04-07, License - CC BY-SA 4.0
-
-function hslToHex(h: number, s: number, l: number): string {
-  l /= 100
-  const a = s * Math.min(l, 1 - l) / 100
-  const f = (n: number): string => {
-    const k = (n + h / 30) % 12
-    const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1)
-    return Math.round(255 * color).toString(16).padStart(2, "0") // convert to Hex and prefix "0" if needed
-  }
-  return `#${f(0)}${f(8)}${f(4)}`
+// HSLだと，色によって背景とのコントラストがかなり違ってしまう
+function oklchToHex(l: number, c: number, h: number): string {
+  const color = new Color("oklch", [l, c, h])
+  return color.to("srgb").toString({ format: "hex" })
 }
